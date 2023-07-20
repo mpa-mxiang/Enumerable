1ebe3af0bd5ac82411ce4b144be4c4404e0e8839
Jiachun Xiang <18576811+mpa-mxiang@users.noreply.github.com>
2023-07-20 15:22:00 -0400

    fix rubocop errors

--- main.rb
+++ main.rb
@@ -1,19 +1,19 @@
 # Create our list
 list = MyList.new(1, 2, 3, 4)
-#<MyList: @list=[1, 2, 3, 4]>
+# <MyList: @list=[1, 2, 3, 4]>
 
 # Test #all?
-list.all? {|e| e < 5}
+list.all? { |e| e < 5 }
 #=> true
-list.all? {|e| e > 5}
+list.all? { |e| e > 5 }
 #=> false
 
 # Test #any?
-list.any? {|e| e == 2}
+list.any? { |e| e == 2 }
 #=> true
-list.any? {|e| e == 5}
+list.any? { |e| e == 5 }
 #=> false
 
 # Test #filter
-list.filter {|e| e.even?}
+list.filter(&:even?)
 #=> [2, 4]
