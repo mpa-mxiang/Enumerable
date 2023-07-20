fd493f3c81d35645f40b426bd03faf52e4525e05
Ali01 <73581663+Ali-0111@users.noreply.github.com>
2023-07-21 00:10:46 +0430

    require_relative added

--- my_list.rb
+++ my_list.rb
@@ -1,13 +1,15 @@
 require_relative 'my_enumerable'
 
 class MyList
+  def each
+    @list.each { |item| yield item }
+  end
+  
   include MyEnumerable
 
   def initialize(*list)
     @list = list
   end
 
-  def each(&block)
-    @list.each(&block)
-  end
+
 end
